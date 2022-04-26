958e93bb0ea366f231ab60d351c0c71d0e80eac5
danielfalqui <100230158+danielfalqui@users.noreply.github.com>
2022-04-25 21:28:49 -0300

    Add files via upload

--- ground.js
+++ ground.js
@@ -1,12 +1,31 @@
 class Ground {
     constructor(x, y, width, height) {
+        var options ={
+            isStatic:true
+        }
+
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.body = Bodies.rectangle(this.x,this.y,this.width,this.height,options);
+        World.add(world,this.body);
+        //adicionar this.body ao mundo
+       
     }
     display(){
+        var pos = this.body.position;//ok
+        push()//ok
+        //usar translate(pos.x,pos.y)
+        translate(pos.x,pos.y);
+        
+        fill("white");
         rectMode(CENTER);
-        rectangle(this.x,this.y,this.width,this.height);
+        // Para exibir você deve usar o rect(x,y,larg,altura) use 0 para x e 0 para y 
+        rect(0,0,this.width,this.height);
+        //Não usar aqui==>rectangle(pos.x,pos.y,pos.width,pos.height);
+       
+        pop()
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,63 +1,74 @@
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Engine = Matter.Engine;//ok
+const World = Matter.World;//ok
+const Bodies = Matter.Bodies;//ok
+const Body = Matter.Body;//ok
 
 var ball,groundObj,leftSide,rightSide;
+//CRIAR VARIÁVEIS world e engine
+var engine,world;
 
-function preload()
+
+/*function preload()
 {
-	
-}
+	não precisa dessa função
+}*/
 
 function setup() {
-	createCanvas(1200, 800);
+	//ajustar tamanho para 1600,700
+	createCanvas(1600, 700);
     
-
-	engine = Engine.create();
-	world = engine.world;
+	engine = Engine.create();//ok
+	world = engine.world;//ok
 
 	//Create the Bodies Here.
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		friction:0,
-		density:1.2
+	var ball_options={//ok
+		isStatic:false,//ok
+		restitution:0.3,//ok
+		friction:0,//ok
+		density:1.2//ok
 	}
-	ball = Bodies.circle(220,10,10,ball_options);
-	World.add(world,ball);
 
-	groundObj=new Ground(width/2,670,width,20);
-	leftSide=new Ground(1100,600,20,120);
-	rightSide=new Ground(1000,600,20,120);
+	ball = Bodies.circle(260,100,20,ball_options);//ok
+	World.add(world,ball);//ok
 
+	groundObj = new Ground(width/2,680,width,20);//ok
+	//ajuste posição y para 610
+	leftSide = new Ground(1100,610,20,120);
+	//ajuste posição x 1350 y 610
+	rightSide = new Ground(1350,610,20,120);
 
-
-	Engine.run(engine);
+	Engine.run(engine);//ok
   
 }
 
 
 function draw() {
-  background(0);
 
-  Engine.update(engine);
-  ellipse(ball.position.x,ball.position.y,10);
-  fill("white");
-  ellipseMode(CENTER);
+	//Usar rectMode por causa dos objetos ground
+	rectMode(CENTER);
+	background(0);
 
+	//ellipse (x,y,larg,alt) o raio é repetido em larg e altura20,20
+	ellipse(ball.position.x,ball.position.y,20,20);
+	fill("white");
+	
+	//ellipseMode(CENTER); não precisa
 
-	  if (keyCode === UP_ARROW){
+	
+	groundObj.display();//ok
+	leftSide.display();//ok
+	rightSide.display();//ok
 
-		Bodies.applyForce(ball,ball.position.x,1);
-	  }
-
-   groundObj.display();
-
-  drawSprites();
+	//drawSprites(); Não temos sprites
  
 }
 
-//pro poderia me ajudar a solucionar o erro?
+//pro eu atualizei mas as coisas ainda estão dando erro porque
 
+function keyPressed(){//ok
+	if (keyCode === UP_ARROW){//ok
+
+	  Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});//ok
+	}
+  }
